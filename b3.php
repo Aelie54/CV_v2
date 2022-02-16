36fd5c505b0b96f84f1d76772ccabaa6bb8c5a79
Amélie Klein <klein.amelie1991@gmail.com>
2022-02-16 23:36:25 +0100

    responsive

--- b3.php
+++ b3.php
@@ -5,67 +5,195 @@
 
 <img src="https://pngimage.net/wp-content/uploads/2018/05/experience-symbol-png-3.png" style="height:60px;margin-bottom:20px">
 
-<div style="align-text:justify">
 
-    <div class="etiquette"><p><span class= "gras" style="color:white">02/2021 - 04/2021 </span></div>
-    <p><span class= "gras">CDD Livreuse Amazon - GP EXRESS (Ludres) :</span><br><br>
-    Opération de chargement, organisation du circuit de livraison et priorité aux impératifs, bonne communication avec les clients, satisfaction client, maîtrise de conduite de camion et des règles de sécurité.</p> 
+    <!-- COllapse accordeon -->
 
+    <div class="accordion accordion-flush" id="accordionFlushExample" style="width:100%">
 
-    <div class="etiquette"><p><span class= "gras" style="color:white">08/2020 - 11/2020 </span></div>
-    <p><span class= "gras"> Apprentie Préparatrice en pharmacie:</span><br><br>
-    Conseil client, réception et gestion des stocks, mise en rayon et étiquetage des produits, préparation des dûs, enregistrements et encaissements, livraisons client à domicile.</p>
-    <br>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-heading1">
+            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#flush-collapse1" aria-expanded="false" aria-controls="flush-collapse1">
+            <span class= "gras">02/2021 - 04/2021 : Livreuse Amazon Prime (GP Express Ludres)</span>
+            </button>
+            </h2>
 
-    <div class="etiquette"><p><span class= "gras" style="color:white">07/2018 - 07/2020 </span></div>
-    <p><span class= "gras"> Préventrice à la Ville de Nancy (Service : RH - Cellule Médecine du Travail) :</span><br><br>
-    Gestion des accidents du travail et des accidents bénins, prévention des risques professionnels, missions de conseil et d’animation.</p>
-    <br>
+            <div id="flush-collapse1" class="accordion-collapse collapse" aria-labelledby="flush-heading1" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Opération de chargement, organisation du circuit de livraison et priorité aux impératifs, bonne communication avec les clients, satisfaction client, maîtrise de conduite de camion et des règles de sécurité.</div>
+            </div>
+        </div>
+        <div class="accordion-item">
 
-    <div class="etiquette"><p><span class= "gras" style="color:white">10/2016 - 08/2018 </span></div>
-    <p><span class= "gras"> Assistante de Service Sécurité et Santé – Wig France Entreprises à Toul (Apprentie et CDD) :</span><br><br>
-    Aide aux certifications, suivi des accidents du travail, rédaction du bilan annuel, suivi d’indicateurs, gestion des dotations en équipements, actions de sensibilisation et d’information, veille réglementaire.</p>
-    <br>
+        <!-- collapse 2-->
 
-    <div class="etiquette"><p><span class= "gras" style="color:white">09/2015 - 07/2016 </span></div>
-    <p><span class= "gras"> Apprentie  - Nancy Métropole - Service Plan Air Climat Energie Territorial :</span><br><br>
-    Rédaction des fiches actions et des chartes d’engagement avec les collectivités, les entreprises et les particuliers, vérification des dossiers de demandes de prime CEE (Certificats d’Economie d’Energie), préparation de la labellisation Cit’Ergie de la ville, rédaction du bilan carbone annuel.</p>
-    <br>
+            <div class="accordion accordion-flush" id="accordionFlushExample" style="width:100%">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-heading2">
+            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#flush-collapse2" aria-expanded="false" aria-controls="flush-collapse2">
+            <span class= "gras">07/2018 - 07/2020 : Préventrice à la Ville de Nancy (service RH - Médecine du Travail)</span>
+            </button>
+            </h2>
 
-    <div class="etiquette"><p><span class= "gras" style="color:white">06/2014 - 06/2016 </span></div>
-    <p><span class= "gras"> Jobs étudiants et Missions d’Interim :</span><br><br>
-    - Accueil téléphonique et réservation de transports en commun (Ville de Metz)<br> 
-    - Cours et aide aux devoirs niveaux collège et lycée (pendant 2 ans)<br> 
-    - Réalisation d'enquêtes à bord des trains SNCF <br>
-    - Travail en usine (type papeterie) <br><br>
-    </p>
+            <div id="flush-collapse2" class="accordion-collapse collapse" aria-labelledby="flush-heading2" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Gestion des accidents du travail et des accidents bénins, prévention des risques professionnels, missions de conseil et d’animation. Organisation et préparation des CHSCT.</div>
+            </div>
+        </div>
+        <div class="accordion-item">
 
-</div>
+        <!-- collapse 3-->
 
-<hr>
+        <div class="accordion accordion-flush" id="accordionFlushExample" style="width:100%">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-heading3">
+            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#flush-collapse3" aria-expanded="false" aria-controls="flush-collapse3">
+            <span class= "gras">10/2016 - 08/2018 :  Assistante de Service Sécurité et Santé – Wig France Entreprises à Toul (Apprentie et CDD)</span>
+            </button>
+            </h2>
+
+            <div id="flush-collapse3" class="accordion-collapse collapse" aria-labelledby="flush-heading3" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Aide aux certifications, suivi des accidents du travail, rédaction du bilan annuel, suivi d’indicateurs, gestion des dotations en équipements, actions de sensibilisation et d’information, veille réglementaire.</div>
+            </div>
+        </div>
+        <div class="accordion-item">
+
+        <!-- collapse 4-->
+
+            <div class="accordion accordion-flush" id="accordionFlushExample" style="width:100%">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-heading4">
+            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#flush-collapse4" aria-expanded="false" aria-controls="flush-collapse4">
+            <span class= "gras">10/4016 - 08/2018 :  Assistante de Service Sécurité et Santé – Wig France Entreprises à Toul (Apprentie et CDD)</span>
+            </button>
+            </h2>
+
+            <div id="flush-collapse4" class="accordion-collapse collapse" aria-labelledby="flush-heading4" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Aide aux certifications, suivi des accidents du travail, rédaction du bilan annuel, suivi d’indicateurs, gestion des dotations en équipements, actions de sensibilisation et d’information, veille réglementaire.</div>
+            </div>
+        </div>
+        <div class="accordion-item">
+
+        <!-- collapse 5-->
+
+        <div class="accordion accordion-flush" id="accordionFlushExample" style="width:100%">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-heading5">
+            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#flush-collapse5" aria-expanded="false" aria-controls="flush-collapse5">
+            <span class= "gras">09/2015 - 07/2016 :  Apprentie  - Nancy Métropole - Service Plan Air Climat Energie Territorial</span>
+            </button>
+            </h2>
+
+            <div id="flush-collapse5" class="accordion-collapse collapse" aria-labelledby="flush-heading5" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Rédaction des fiches actions et des chartes d’engagement avec les collectivités, les entreprises et les particuliers, vérification des dossiers de demandes de prime CEE (Certificats d’Economie d’Energie), préparation de la labellisation Cit’Ergie de la ville, rédaction du bilan carbone annuel.</div>
+            </div>
+        </div>
+        <div class="accordion-item">
+
+            <!-- collapse 6-->
+
+            <div class="accordion accordion-flush" id="accordionFlushExample" style="width:100%">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-heading6">
+            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#flush-collapse6" aria-expanded="false" aria-controls="flush-collapse6">
+            <span class= "gras">06/2014 - 06/2016 :  Jobs étudiants et Missions d’Interim</span>
+            </button>
+            </h2>
+
+            <div id="flush-collapse6" class="accordion-collapse collapse" aria-labelledby="flush-heading6" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Jobs étudiants et Missions d’Interim :</span><br><br>
+            - Accueil téléphonique et réservation de transports en commun (Ville de Metz)<br> 
+            - Cours et aide aux devoirs niveaux collège et lycée (pendant 2 ans)<br> 
+            - Réalisation d'enquêtes à bord des trains SNCF <br>
+            - Travail en usine (type papeterie) <br><br></div>
+            </div>
+        </div>
+        <div class="accordion-item">
+        <!-- FIN-->
+    </div>
+        
+<br><br><br>
+
 
 <div>
-<h1> Compétences aquises </h1>
+    <h1> Compétences aquises </h1>
 </div>
 
+<div style="align-text:center; text-align: center;"> 
 <img src="https://pngimage.net/wp-content/uploads/2018/05/comp%C3%A9tences-icone-png.png" style="height:60px;margin-bottom:20px">
-
-<div style="align-text:justify">
-
-    <div class="etiquette"><p><span class= "gras" style="color:white">Informatique et Linguistique :</span></div>
-    <p>Pack office (Word, Excel, Publisher, PowerPoint) et Anglais (niveau scolaire)</p> 
-
-    <div class="etiquette"><p><span class= "gras" style="color:white">Administratif :</span></div>
-    <p>Suivi des dossiers, archivages, enregistrements, suivi des indicateurs, préparation et organisation des réunions, gestion des appels, des mails, des plannings et du courrier et rédaction d'écrits divers (comptes rendus, procès verbaux, bilans annuels)</p>
-
-    <div class="etiquette"><p><span class= "gras" style="color:white">Conseil - Animation - Savoir Etre :</span></div>
-    <p>Accueil et conseil client, accueil des nouveaux arrivants dans une entreprise (présentations, culture d’entreprise, certifications, objectifs ect), aide à la sensibilisation, réalisation d’enquêtes accidents du travail, présentation des dossiers, conseil sécurité, animation de réunions, comités, visites d'ateliers et de chantiers. Missions réalisées avec divers intervenants et collaborateurs : Inspection du travail, Carsat, Médecine du Travail, syndicats, directeurs de pôles, chefs de services, Police Municipale, agents techniques, des écoles, de restauration, d’entretien et d'ateliers, sous-traitants et fournisseurs.</p>
-
-    <div class="etiquette"><p><span class= "gras" style="color:white">Compétences transverses :</span></div>
-    <p>Gestion du stress, ponctualité, respect des délais, organisation et planification, capacité d’analyse, de synthèse et de communication, sens des responsabilité, culture d’entreprise, autonomie, bon relationnel </p>
-
 </div>
 
-<input class="favorite styled b5" type="button" id="bouton4"  onclick="run5()" value="Retour">
 
-<br><hr>
+            <!-- COllapse accordeon -->
+
+        <div class="accordion accordion-flush" id="accordionFlushExample" style="width:100%">
+
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-heading1">
+            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#flush-collapse1" aria-expanded="false" aria-controls="flush-collapse1">
+            <span class= "gras">Informatique et Linguistique</span>
+            </button>
+            </h2>
+
+            <div id="flush-collapse1" class="accordion-collapse collapse" aria-labelledby="flush-heading1" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Pack office (Word, Excel, Publisher, PowerPoint) et Anglais (niveau scolaire)</div>
+            </div>
+        </div>
+        <div class="accordion-item">
+
+        <!-- collapse 2-->
+
+            <div class="accordion accordion-flush" id="accordionFlushExample" style="width:100%">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-heading2">
+            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#flush-collapse2" aria-expanded="false" aria-controls="flush-collapse2">
+            <span class= "gras">Administratif</span>
+            </button>
+            </h2>
+
+            <div id="flush-collapse2" class="accordion-collapse collapse" aria-labelledby="flush-heading2" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Suivi des dossiers, archivages, enregistrements, suivi des indicateurs, préparation et organisation des réunions, gestion des appels, des mails, des plannings et du courrier et rédaction d'écrits divers (comptes rendus, procès verbaux, bilans annuels)</div>
+            </div>
+        </div>
+        <div class="accordion-item">
+
+        <!-- collapse 3-->
+
+        <div class="accordion accordion-flush" id="accordionFlushExample" style="width:100%">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-heading3">
+            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#flush-collapse3" aria-expanded="false" aria-controls="flush-collapse3">
+            <span class= "gras">Conseil - Animation - Savoir Etre</span>
+            </button>
+            </h2>
+
+            <div id="flush-collapse3" class="accordion-collapse collapse" aria-labelledby="flush-heading3" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Accueil et conseil client, accueil des nouveaux arrivants dans une entreprise (présentations, culture d’entreprise, certifications, objectifs ect), aide à la sensibilisation, réalisation d’enquêtes accidents du travail, présentation des dossiers, conseil sécurité, animation de réunions, comités, visites d'ateliers et de chantiers. Missions réalisées avec divers intervenants et collaborateurs : Inspection du travail, Carsat, Médecine du Travail, syndicats, directeurs de pôles, chefs de services, Police Municipale, agents techniques, des écoles, de restauration, d’entretien et d'ateliers, sous-traitants et fournisseurs.</p></div>
+            </div>
+        </div>
+        <div class="accordion-item">
+
+        <!-- collapse 4-->
+
+            <div class="accordion accordion-flush" id="accordionFlushExample" style="width:100%">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-heading4">
+            <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#flush-collapse4" aria-expanded="false" aria-controls="flush-collapse4">
+            <span class= "gras">Compétences transverses</span>
+            </button>
+            </h2>
+
+            <div id="flush-collapse4" class="accordion-collapse collapse" aria-labelledby="flush-heading4" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Gestion du stress, ponctualité, respect des délais, organisation et planification, capacité d’analyse, de synthèse et de communication, sens des responsabilité, culture d’entreprise, autonomie, bon relationnel </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            
+        </div>
+    
+    <br><br>
+    
+    <div style="align-text:center; text-align: center;"> 
+    <input class="favorite styled b5" type="button" id="bouton4"  onclick="run5()" value="Retour">
+    </div> 
+        
+<br><br>
+        
+        
--- b4.php
+++ b4.php
@@ -6,7 +6,7 @@
 
         <div style="text-align:justify">
 
-            <p> Je suis actuellement à la recher d'un stage en entreprise d'une durée de trois semaines prévu du 16 mai au 8 juin. <br><br>
+            <p> Je suis actuellement à la recherche d'un stage en entreprise d'une durée de trois semaines, prévu du 16 mai au 8 juin. <br><br>
 
             Si mon profil vous intéresse, n'hésitez pas à me <span><a style="text-decoration:underline; font-weight: bold; color:orange font-weight: bold; color:orange;" href="mailto:klein.amelie1991@gmail.com"> Contacter</a> ! Ce serait avec plaisir que je me rendrais dsponible pour un eventuel entretien. <br><br> </p>
 
@@ -20,9 +20,11 @@
         
         <p>Vous pouvez également télécharger mon CV en version PDF : <br><br></p>
 
+        <div style="align-text:center; text-align: center;"> 
         <a href="CV_KLEINAmelie.pdf" target="_blank" type="button" id="retour"  style="border:none; background-color:transparent;" href="mailto:klein.amelie1991@gmail.com"> 
             <img style="height:80px;text-align:center; border-radius:30px;display: block;margin: auto;" alt=" PSst! par ici ! " src="https://www.bag.admin.ch/bag/fr/home/krankheiten/ausbrueche-epidemien-pandemien/aktuelle-ausbrueche-epidemien/novel-cov/downloads-in-verschiedenen-sprachen/downloads/_jcr_content/image.imagespooler.png/1622446996805/588.1000/Icon%20Downloads%20klein.png">
         </a> 
+    </div>
             
             <br> <br> <input style="display: block;margin: auto;" class="favorite styled b5" type="button" id="bouton4"  onclick="run5()" value="Retour">
 
