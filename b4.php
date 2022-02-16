593cb937c6acd59028999182a44c6edfd99049c0
Aelie54 <klein.amelie1991@gmail.com>
2022-02-16 10:16:05 +0100

    nouvelle_branche_a_la_con

--- b4.php
+++ b4.php
@@ -4,25 +4,25 @@
             <h1>Recherche de Stage</h1>
         </div>
 
-        <div style="align-text:justify">
+        <div style="text-align:justify">
 
             <p> Je suis actuellement à la recher d'un stage en entreprise d'une durée de trois semaines prévu du 16 mai au 8 juin. <br><br>
 
-            Si mon profil vous intéresse, n'hésitez pas à me contacter ! Ce serait avec plaisir que je me rendrais dsponible pour un eventuel entretien. <br><br> </p>
+            Si mon profil vous intéresse, n'hésitez pas à me <span><a style="text-decoration:underline; font-weight: bold; color:orange font-weight: bold; color:orange;" href="mailto:klein.amelie1991@gmail.com"> Contacter</a> ! Ce serait avec plaisir que je me rendrais dsponible pour un eventuel entretien. <br><br> </p>
 
-            <div style="align-text:center; ">
+            <div style="text-align:center; ">
             <button type="button" id="retour"  style="border:none; background-color:transparent;display: block;margin: auto;"> 
-                <img style="height:150px;text-align:center" alt=" PSst! par ici ! " src="https://www.zohowebstatic.com/sites/default/files/mail/manage-multiple-assests.png">
+                <img style="height:150px;text-align:center" alt=" PSst! par ici ! " src="https://www.zohowebstatic.com/sites/default/files/mail/manage-multiple-assests.png" >
             </button> 
         </div>
         
         <br><br>
         
         <p>Vous pouvez également télécharger mon CV en version PDF : <br><br></p>
-        
-        <button type="button" id="retour"  style="border:none; background-color:transparent;display: block;margin: auto;"> 
-            <img style="height:80px;text-align:center; border-radius:30px;" alt=" PSst! par ici ! " src="https://www.bag.admin.ch/bag/fr/home/krankheiten/ausbrueche-epidemien-pandemien/aktuelle-ausbrueche-epidemien/novel-cov/downloads-in-verschiedenen-sprachen/downloads/_jcr_content/image.imagespooler.png/1622446996805/588.1000/Icon%20Downloads%20klein.png">
-        </button> 
+
+        <a href="CV_KLEINAmelie.pdf" target="_blank" type="button" id="retour"  style="border:none; background-color:transparent;" href="mailto:klein.amelie1991@gmail.com"> 
+            <img style="height:80px;text-align:center; border-radius:30px;display: block;margin: auto;" alt=" PSst! par ici ! " src="https://www.bag.admin.ch/bag/fr/home/krankheiten/ausbrueche-epidemien-pandemien/aktuelle-ausbrueche-epidemien/novel-cov/downloads-in-verschiedenen-sprachen/downloads/_jcr_content/image.imagespooler.png/1622446996805/588.1000/Icon%20Downloads%20klein.png">
+        </a> 
             
             <br> <br> <input style="display: block;margin: auto;" class="favorite styled b5" type="button" id="bouton4"  onclick="run5()" value="Retour">
 
